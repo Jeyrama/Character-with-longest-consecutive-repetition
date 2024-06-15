463649b47abd3d4aef8bb1226a2cf0da454c449e
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-06-14 22:02:03 -0700

    Update Character-with-longest-consecutive-repetition.js

--- Character-with-longest-consecutive-repetition.js
+++ Character-with-longest-consecutive-repetition.js
@@ -37,3 +37,19 @@ function longestRepetition(s) {
 }
 
 // or
+
+const longestRepetition = s => {
+  let max = 0;
+  let char = ''; 
+  for (let i = 1, l = s.length, c = 1; i <= l; i++) {
+    if (s[i] === s[i - 1]) c++;
+    else {
+      if (c > max) {
+        max = c;
+        char = s[i - 1];   
+      }
+      c = 1;
+    }
+  }
+  return [char, max];
+}
