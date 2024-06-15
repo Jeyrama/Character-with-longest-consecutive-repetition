8c0e8b24757df141e82c2d6f49a46d1591ae2fb3
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-06-14 22:01:43 -0700

    Update Character-with-longest-consecutive-repetition.js

--- Character-with-longest-consecutive-repetition.js
+++ Character-with-longest-consecutive-repetition.js
@@ -13,3 +13,25 @@ For empty string return:
 
 
 // Solution
+
+function longestRepetition(s) {
+  let count = 0;
+  let prevLetter = '';
+  
+  return s.toLowerCase().split('').reduce((acc, curr) => {
+    if(curr === prevLetter){
+      count++;
+    }
+    else{
+      count = 1;
+    }
+
+    if(count > acc[1]){
+      acc[1] = count;
+      acc[0] = curr;
+    }
+
+    prevLetter = curr;
+    return acc;
+  }, ['', 0]);
+}
